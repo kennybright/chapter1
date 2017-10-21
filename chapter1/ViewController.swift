957d9d4ebe6cf65ddee7d3ae51fae0cb452956aa
Kenny Bright <kennybright@hotmail.com>
2017-10-21 14:08:28 -0700

    Initial Commit

---
+++ chapter1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chapter1
+//
+//  Created by Kenny Bright on 10/21/17.
+//  Copyright © 2017 Kenny Bright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
